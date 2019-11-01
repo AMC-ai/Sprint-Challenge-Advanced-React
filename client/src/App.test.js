36b1c7e30aa9a8a1ba0a6c32986acae8dc008950
AMC <53350942+AMC-ai@users.noreply.github.com>
2019-11-01 14:03:11 -0400

    testing

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+// import '@testing-library/jest-dom/extend-expect'
+import { render, fireEvent } from '@testing-library/react';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+test('renders', () => {
+  render(<App />);
 });
+
+// it('renders without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<App />, div);
+//   ReactDOM.unmountComponentAtNode(div);
+// });
+
+test('button id', () => {
+  const { getByTestId } = render(<App />)
+  getByTestId(`on-switch`)
+
+})
